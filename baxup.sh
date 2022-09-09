eb5d5d2ed92ea08cf8f4ba8dfede568ce2daafc3
admin <contact@aliberksandikci.com.tr>
2022-09-10 00:44:40 +0300

    Initial Commit

--- baxup.sh
+++ baxup.sh
@@ -13,16 +13,16 @@ msgHelp="Usage: baxup [OPTIONS]
 Backups files specified in backup-dir.txt
   [NI] => Not Implemented
   -d, --debug                do not copy files, work with empty folder
-  [NI] -r, --root                 ignore the required sudo permisson ${colorRed}-Dangerous-${colorReset}
-  [NI] -k, --keep                 keep unarchived folder
-  [NI] -c, --create               create a new compressed backup archive
-  [NI] -s, --setup=PATH           extract target archive and move to root for merge
+  -r, --root                      ignore the required sudo permisson ${colorRed}-Dangerous-${colorReset}
+  -k, --keep                      keep unarchived folder
+  -c, --create                    create a new compressed backup archive
+  -s, --setup=PATH                extract target archive and move to root for merge
   -v, --verbose                   verbosely list files processed and logs
-  [NI] -l, --log                  output logs
-  [NI] -f, --frequency=NUMBER     set a frequency for baxup (1 time each NUMBER day)             
+  -l, --log                       output logs
+  -f, --frequency=NUMBER          set a frequency for baxup (1 time each NUMBER day)             
   -h, --help                      display this help message
 
-  [NI] --startup                  special commands for startup
+  --startup                       special commands for startup
                                   (checks frequency and automatically creates backup)
   [NI] --dir-path=PATH            specify a path for backup-dir.txt
   [NI] --target-path=PATH         specify a path where backups will be stored
@@ -30,19 +30,23 @@ ${colorCyan}Copyright 2022 © Aliberk Sandıkçı${colorReset}\n"
 # msgExample='msg'
 
 boolDebug=0
-# boolRoot=0
-# boolKeep=0
-# boolCreate=0
+boolRoot=0
+boolKeep=0
+boolCreate=0
+boolSetup=0
 boolVerbose=0
-# boolLog=0
+boolLog=0
+boolFrequency=0
 boolHelp=0
-# boolStartup=0
+boolStartup=0
 
-# varFrequency=0
+varFrequency=0
 varUser=$([ -n "$SUDO_USER" ] && echo "$SUDO_USER" || echo "$USER")
+
 pathDir="/home/${varUser}/backups/backup-dir.txt"
 pathHistory="/home/${varUser}/backups/backup-history"
 pathTarget="/home/${varUser}/backups/"
+pathSetup=""
 #-!SECTION VARIABLES
 
 _help() {
@@ -55,18 +59,39 @@ _check_args() {
   else
     for ((i = 1; i <= $#; i++)); do
       # look for more info: LINK https://unix.stackexchange.com/a/631737
-      case "${!i}" in
-      "--help" | "-h")
-        boolHelp=1
-        ;;
-      "--verbose" | "-v")
-        boolVerbose=1
-        ;;
-      "--debug" | "-d")
+      tmpVar="${!i}"
+
+      if [[ $tmpVar == "--debug" || $tmpVar == "-d" ]]; then
         boolDebug=1
-        ;;
-      esac
+      elif [[ $tmpVar == "--root" || $tmpVar == "-r" ]]; then
+        boolRoot=1
+      elif [[ $tmpVar == "--keep" || $tmpVar == "-k" ]]; then
+        boolKeep=1
+      elif [[ $tmpVar == "--create" || $tmpVar == "-c" ]]; then
+        boolCreate=1
+      elif [[ $tmpVar == "--setup="* || $tmpVar == "-s="* ]]; then
+        boolSetup=1
+        pathSetup=${tmpVar#*=}
+      elif [[ $tmpVar == "--verbose" || $tmpVar == "-v" ]]; then
+        boolVerbose=1
+      elif [[ $tmpVar == "--log" || $tmpVar == "-l" ]]; then
+        boolLog=1
+      elif [[ $tmpVar == "--frequency="* || $tmpVar == "-f="* ]]; then
+        boolFrequency=1
+        varFrequency=${tmpVar#*=}
+      elif [[ $tmpVar == "--help" || $tmpVar == "-h" ]]; then
+        boolHelp=1
+      elif [[ $tmpVar == "--startup" ]]; then
+        boolStartup=1
+      else
+        printf '%b%s%b%s%b' "$colorRed" "Error: " "$colorReset" "There is no command named $tmpVar or $tmpVar do not have enough argument" "\n"
+        sleep 0.1
+        printf '%b%s%b' "$colorRed" "Aborting..." "$colorReset\n"
+        exit
+      fi
+
     done
+
   fi
 
 }
@@ -77,6 +102,19 @@ if [ $boolHelp == 1 ]; then
   _help
   exit
 elif [ $boolDebug == 1 ]; then
-  echo "$varUser"
-
+  echo "-Temporary Debug Scripts-"
+  echo -e "Variables Currently not in usage:\n"
+  echo "$boolRoot"
+  echo "$boolKeep"
+  echo "$pathSetup"
+  echo "$boolVerbose"
+  echo "$boolLog"
+  echo "$boolFrequency"
+  echo "$varFrequency"
+  echo "$boolStartup"
+  echo "$pathDir"
+  echo "$pathHistory"
+  echo "$pathTarget"
+elif [[ $boolCreate == 1 && $boolSetup == 1 ]]; then
+  printf '%b%s%b%s%b' "$colorRed" "Warning: " "$colorReset" "Do not use both create and setup commands" "\n"
 fi
