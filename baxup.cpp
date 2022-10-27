356e1c54a5d4f75716dffb3f1eaf75aa98abd716
Aliberk Sandıkçı <git@aliberksandikci.com.tr>
2022-10-27 20:32:54 +0300

    refactor(language): start to change language c++ from shell
    - Create file named `baxup.cpp`
    
    - Add mini license to `baxup.cpp`
    
    - Start to convert `baxup.sh` to `baxup.cpp`

---
+++ baxup.cpp
@@ -0,0 +1,28 @@
+/* baxup -- backup, setup, freeze or restore
+   Copyright (C) 2022 Aliberk Sandıkçı
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Aliberk Sandıkçı <git@aliberksandikci.com.tr>  */
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+
+#define PROGRAM_NAME "baxup"
+
+#define AUTHORS proper_name("Aliberk Sandıkçı")
+// TODO - What proper_name function does
+
+int main(int argc, char **argv) {}
